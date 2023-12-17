2cf2ece9d8216e5395a85688375e0afa5cd2931e
mohamedbentaleb <47635527+mohamedbentaleb@users.noreply.github.com>
2023-12-17 18:20:36 +0100

    add template

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,223 @@
+    <!-- Footer -->
+    <footer class="site-footer footer-image" style="">
+        <div class="container">
+             <div class="footer-top">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 col-xs-6 footer-col-4">
+                        <div class="widget widget_about">
+                            <div class="logo-footer m-a0"><a href="index.html"><img src="images/logo-light1.png" alt=""></a></div>
+                            <p class="m-t20 m-b40">We know the difference is in the details and that’s why our car rental services, in the tourism and business industry, stand out for their quality.</p>
+							<form action="script/mailchamp.php" method="post" class="form-subscribe dzSubscribe">
+								<div class="dzSubscribeMsg"></div>
+								<div class="input-group m-b15">
+									<input name="dzEmail" required="required" type="email" class="form-control" placeholder="Enter Your Email">
+									<span class="input-group-btn">
+										<button name="submit" value="Submit" type="submit" class="site-button btn-block">
+											<i class="fa fa-envelope font-18"></i>
+										</button>
+									</span>
+								</div>
+							</form>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6 footer-col-4">
+                        <div class="widget widget_services">
+							<div class="contact-line">
+								<div class="text-primary text-uppercase font-14 font-weight-500 m-b5"><i class="fa fa-phone m-r5"></i> call us on Line 1</div>
+								<div class="contact-no font-weight-600">95747 47304</div>
+							</div>
+                            <h4 class="m-b15 text-uppercase">Useful Links</h4>
+                            <ul>
+                                <li><a href="new-car-latest.html">Latest Cars</a></li>
+                                <li><a href="new-car-upcoming.html">Upcoming Cars</a></li>
+                                <li><a href="used-car-search.html">Search Used Car</a></li>
+                                <li><a href="used-car-sell.html">Car Sell</a></li>
+                                <li><a href="compare-car.html">Compare Car</a></li>
+                            </ul>
+                        </div>
+                    </div>
+					<div class="col-md-3 col-sm-6 col-xs-6 footer-col-4">
+                        <div class="widget widget_services">
+							<div class="contact-line">
+								<div class="text-primary text-uppercase font-14 font-weight-500 m-b5"><i class="fa fa-phone m-r5"></i> call us on Line 2</div>
+								<div class="contact-no font-weight-600">63580 43699</div>
+							</div>
+                            <h4 class="m-b15 text-uppercase">Our Info</h4>
+							<ul>
+                                <li><a href="page-about.html">About us</a></li>
+                                <li><a href="page-privacy-policy.html">Privacy Policy</a></li>
+                                <li><a href="used-car-search.html">Services</a></li>
+                                <li><a href="page-services.html">Career</a></li>
+                                <li><a href="page-help.html">Help</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6 footer-col-4">
+                        <div class="widget widget_services">
+							<div class="map-line">
+								<a href="javascript:;" class="site-button">
+									<img src="images/map-icon.png" alt=""/>
+									<span class="m-l10 m-t10">Car Dealers on Map</span>
+								</a>
+							</div>
+                            <h4 class="m-b15 text-uppercase">Account Information</h4>
+							<ul>
+                                <li><a href="page-about.html">About us</a></li>
+                                <li><a href="page-privacy-policy.html">Privacy Policy</a></li>
+                                <li><a href="used-car-search.html">Services</a></li>
+                                <li><a href="page-services.html">Career</a></li>
+                                <li><a href="page-help.html">Help</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- footer bottom part -->
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 col-xs-12 text-left copyright-text"> © Copyright 2021 Car Dealer Developed By <a href="http://dexignlab.com/" target="_blank">DexignLab</a> </div>
+                    <div class="col-md-6 col-sm-6 col-xs-12 text-right social-item-link">
+						<a href="#" class="fb"><i class="fa fa-facebook"></i>. Facebook </a>
+						<a href="#" class="twt"><i class="fa fa-twitter"></i>. Tweeter </a>
+						<a href="#" class="gplus"><i class="fa fa-google-plus"></i>. Google </a>
+					</div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer END-->
+    <!-- scroll top button -->
+    <button class="scroltop fa fa-chevron-up" ></button>
+<!-- JavaScript  files ========================================= -->
+<script  src="{{ asset('assets/js/jquery.min.js') }}"></script>
+<!-- jquery.min js -->
+<script src="{{ asset('assets/js/wow.js') }}"></script>
+<!-- wow.min js -->
+<script  src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<!-- bootstrap.min js -->
+<script  src="{{ asset('assets/js/bootstrap-select.min.js') }}"></script>
+<!-- Form js -->
+<script  src="{{ asset('assets/js/jquery.bootstrap-touchspin.js') }}"></script>
+<!-- Form js -->
+<script  src="{{ asset('assets/js/magnific-popup.js') }}"></script>
+<!-- magnific-popup js -->
+<script  src="{{ asset('assets/js/waypoints-min.js') }}"></script>
+<!-- waypoints js -->
+<script  src="{{ asset('assets/js/counterup.min.js') }}"></script>
+<!-- counterup js -->
+<script src="{{ asset('assets/js/imagesloaded.js') }}"></script>
+<!-- masonry  -->
+<script src="{{ asset('assets/js/masonry-3.1.4.js') }}"></script>
+<!-- masonry  -->
+<script src="{{ asset('assets/js/masonry.filter.js') }}"></script>
+<!-- masonry  -->
+<script  src="{{ asset('assets/js/owl.carousel.js') }}"></script>
+<!-- OWL  Slider  -->
+<script  src="{{ asset('assets/js/owl.linked.js') }}"></script>
+<!-- OWL  Slider  -->
+<script  src="{{ asset('assets/js/dz.carousel.js') }}"></script>
+<!-- sortcode fuctions  -->
+<script  src="{{ asset('assets/js/dz.ajax.js') }}"></script>
+
+<script src="{{ asset('assets/js/modernizr.custom.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.hoverdir.js') }}"></script>
+<script  src="{{ asset('assets/js/custom.min.js') }}"></script>
+<!-- custom fuctions  -->
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBjirg3UoMD5oUiFuZt3P9sErZD-2Rxc68&callback=initMap"></script>
+<script>
+var map, popup, Popup;
+/** Initializes the map and the custom popup. */
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 25.222690, lng: 75.904760},
+		zoom: 10,
+		disableDefaultUI: true,
+		styles:[{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#d1c7b3"}]},{"featureType":"transit","stylers":[{"color":"#808080"},{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"weight":1.8}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ebebeb"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"color":"#a7a7a7"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#eeebe4"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"color":"#737373"}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#d6d6d6"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#dadada"}]}]
+	});
+
+
+	var image = 'images/location.png';
+	var beachMarker = new google.maps.Marker({
+		position: {lat: 25.222690, lng: 75.904760},
+		map: map,
+		icon: image
+	});
+		Popup = createPopupClass();
+		popup = new Popup(
+			new google.maps.LatLng(25.222690, 75.904760),
+			document.getElementById('content'));
+		popup.setMap(map);
+	}
+
+/**
+ * Returns the Popup class.
+ *
+ * Unfortunately, the Popup class can only be defined after
+ * google.maps.OverlayView is defined, when the Maps API is loaded.
+ * This function should be called by initMap.
+ */
+function createPopupClass() {
+  /**
+   * A customized popup on the map.
+   * param {!google.maps.LatLng} position
+   * param {!Element} content The bubble div.
+   * constructor
+   * extends {google.maps.OverlayView}
+   */
+  function Popup(position, content) {
+    this.position = position;
+
+    content.classList.add('popup-bubble');
+
+    // This zero-height div is positioned at the bottom of the bubble.
+    var bubbleAnchor = document.createElement('div');
+    bubbleAnchor.classList.add('popup-bubble-anchor');
+    bubbleAnchor.appendChild(content);
+
+    // This zero-height div is positioned at the bottom of the tip.
+    this.containerDiv = document.createElement('div');
+    this.containerDiv.classList.add('popup-container');
+    this.containerDiv.appendChild(bubbleAnchor);
+
+    // Optionally stop clicks, etc., from bubbling up to the map.
+    google.maps.OverlayView.preventMapHitsAndGesturesFrom(this.containerDiv);
+  }
+  // ES5 magic to extend google.maps.OverlayView.
+  Popup.prototype = Object.create(google.maps.OverlayView.prototype);
+
+  /** Called when the popup is added to the map. */
+  Popup.prototype.onAdd = function() {
+    this.getPanes().floatPane.appendChild(this.containerDiv);
+  };
+
+  /** Called when the popup is removed from the map. */
+  Popup.prototype.onRemove = function() {
+    if (this.containerDiv.parentElement) {
+      this.containerDiv.parentElement.removeChild(this.containerDiv);
+    }
+  };
+
+  /** Called each frame when the popup needs to draw itself. */
+  Popup.prototype.draw = function() {
+    var divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
+
+    // Hide the popup when it is far out of view.
+    var display =
+        Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
+        'block' :
+        'none';
+
+    if (display === 'block') {
+      this.containerDiv.style.left = divPosition.x + 'px';
+      this.containerDiv.style.top = divPosition.y + 'px';
+    }
+    if (this.containerDiv.style.display !== display) {
+      this.containerDiv.style.display = display;
+    }
+  };
+
+  return Popup;
+}
+</script>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,28 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <meta name="robots" content="" />
+    <meta name="description" content="" />
+    <meta name="format-detection" content="telephone=no">
+    <!-- Favicons Icon -->
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+    <!-- Page Title Here -->
+    <title>CarZone - Car Dealer Automotive Responsive HTML5 Template</title>
+    <!-- Mobile Specific -->
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <!--[if lt IE 9]>
+            <script src="js/html5shiv.min.js"></script>
+            <script src="js/respond.min.js"></script>
+        <![endif]-->
+    <!-- Stylesheets -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/plugins.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/templete.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/magnific-popup.css') }}">
+    <link class="skin" rel="stylesheet" type="text/css" href="{{ asset('assets/css/skin/skin-1.css') }}">
+
+    <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900|Open+Sans:300,400,600,700,800|Raleway:100,200,300,400,500,600,700,800,900|Poppins:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    </head>
